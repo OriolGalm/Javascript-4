f32197d5fae960dc3e0317b70b76bb28147a13db
OriolGalm <accions018@gmail.com>
2022-06-20 13:02:23 +0200

    Primer commit

--- src/films.js
+++ src/films.js
@@ -1,28 +1,40 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  let result =  array.map(item => item.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter(x => x.director === "Sidney Lumet" );
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  let result = array.filter(x => x.director === "Sidney Lumet" );
+  let sumaNota = result.map(r => r.score).reduce((n, s) =>  n + s);
+  let notaMitja = (sumaNota / result.length).toFixed(2);
+  console.log("EXERCICE 3 ->", notaMitja);
+  return notaMitja;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let result = array.map(x => x.title).sort().slice(0, 20);
+  console.log("EXERCICE 4 ->", result);
+  return result;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  let result = array.filter(x => x.year == x.year).sort((a, b) => a - b).reverse();
+  //let igual = result.filter(i => i.year == )
+  //let igual = result.map().reduce((y, n) => y.year == n.year);
+  console.log("EXERCICE 5 ->", result);
+  return result;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
