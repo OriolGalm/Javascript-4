46ce54b1248861a6c245cceebc7626ce2cc8bbb3
OriolGalm <accions018@gmail.com>
2022-06-27 13:04:43 +0200

    repas 1

--- src/films.js
+++ src/films.js
@@ -1,14 +1,14 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
   let result =  array.map(item => item.director);
-  console.log("EXERCICE 1 ->", result);
+  //console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
   let result = array.filter(x => x.director === director );
-  console.log("EXERCICE 2 ->", result);
+  //console.log("EXERCICE 2 ->", result);
   return result;
 }
 
@@ -35,29 +35,43 @@ function orderByYear(array) {
     if (a.title > b.title) return 1;
     if (a.title < b.title) return -1;
   });
-  console.log("EXERCICE 5 ->", result);
+  //console.log("EXERCICE 5 ->", result);
   return result;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory(array, genreType) {
-  let result = array.filter(x => x.genre[genreType] === x.genre[genreType]);
-  let genreAvarage = result.map(r => r.score).reduce((n, s) => n + s) / result.length.toFixed(2);
-   console.log("EXERCICE 6 ->", genreAvarage);
-  return genreAvarage;
+  for(let i = 0; i< array.length; i++){
+    if(array[i].genre == genreType  && array[i].score != ''){
+      let result = array.filter(x => x.genre[genreType] === x.genre[genreType]);
+      let genreAvarage = result.map(r => r.score).reduce((n, s) => n + s) / result.length.toFixed(2);
+      console.log("EXERCICE 6 ->", array[i].genre + ", " + genreAvarage);
+      return genreAvarage;
+    }
+  }
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-  let minuts = movies.map(m => m.duration.replace(/\D/g,''));
+function hoursToMinutes(array) {
+  let minutsTotal = [];
+  let minuts = array.map(m => m.duration.replace(/\D/g,''));
   let calc = minuts.map(c => (c[0]*60) + parseInt(c[1] + c[2]));
-  let prop = 'duration';
-  movies[prop] = minuts;
+  for(let i = 0; i < array.length; i++){
+    array[i].duration = calc[i];
+    minutsTotal = array;
+  };
+  //console.log("EXERCICE 7 ->", minutsTotal);
+  return minutsTotal;
 }
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+  /* let result = array.sort((a, b) => {
+    if (a.year > b.year) return 1;
+    if (a.year < b.year) return -1;
+  }).filter(x => x.score == Math.max(x.score));
+  console.log("EXERCICE 8 ->", result);
+  return result; */
 }
 
 
